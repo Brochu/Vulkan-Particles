03011622f4f25799d1d1b8afa3c17df7504a625a
Brochu <alexbrochu1@gmail.com>
2021-06-24 14:21:19 -0400

    Better compute shader implementation to show particle movements

--- shaders/comp.glsl
+++ shaders/comp.glsl
@@ -6,18 +6,29 @@ layout(set = 0, binding = 0, std430) buffer Positions
     vec4 pos[];
 } positions;
 
+float rand(vec2 co)
+{
+    return fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453) - 0.5;
+}
+
 void main()
 {
     vec4 current_pos = positions.pos[gl_GlobalInvocationID.x];
+    float xmovement = rand(current_pos.xy + 1);
+    float ymovement = rand(current_pos.xy + 5.0);
 
     //vec2 velocity = velocities.vel[gl_GlobalInvocationID.x];
     //current_pos += velocity;
+    current_pos.x += xmovement;
+    current_pos.y += ymovement;
 
-    //if (current_pos.x > 0.95 || current_pos.x < -0.95 ||
-    //    current_pos.y > 0.95 || current_pos.y < -0.95)
-    //{
-    //    current_pos = -2.0 * velocity + current_pos * 0.05;
-    //}
+    if (current_pos.x > 50.95 || current_pos.x < -50.95 ||
+        current_pos.y > 50.95 || current_pos.y < -50.95)
+    {
+        //current_pos = -2.0 * velocity + current_pos * 0.05;
+        current_pos.x = 0;
+        current_pos.y = 0;
+    }
 
-    positions.pos[gl_GlobalInvocationID.x] = current_pos + vec4(1, 0, 0, 0);
+    positions.pos[gl_GlobalInvocationID.x] = current_pos;
 }
